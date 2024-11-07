571ed6a518fcce336135f6b449ba4a3b444efcf3
Maria Vladykin <maria_vladykin@gmx.de>
2024-11-07 08:54:13 +0100

    chore: reeformatted dataset comments, added total number of images

--- helpers/split.py
+++ helpers/split.py
@@ -11,13 +11,13 @@ def train_test_split() -> None:
     # furthermore all categories have about the same amount of images
     # this lowers biases of the CNN
     # fmt:off
-    TREE_SPECIES: list[str] = [     # total, train/test
-        "Adenanthera microsperma",  # 80,    73   / 7
-        "Cananga odorata",          # 101,   91   / 10
-        "Cedrus",                   # 93,    84   / 9
-        "Cocos nucifera",           # 110,   100  / 10
-        "Dalbergia oliveri",        # 89,    81   / 8
-    ]
+    TREE_SPECIES: list[str] = [     # total | train / test
+        "Adenanthera microsperma",  # 80    | 73    / 7
+        "Cananga odorata",          # 101   | 91    / 10
+        "Cedrus",                   # 93    | 84    / 9
+        "Cocos nucifera",           # 110   | 100   / 10
+        "Dalbergia oliveri",        # 89    | 81    / 8       
+    ]                               # 473   | 429   / 44
     # fmt:on
 
     for species in TREE_SPECIES:
