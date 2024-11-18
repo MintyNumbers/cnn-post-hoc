881f386763248e2ef0e4c03c46f355737f0bdf3a
Maria Vladykin <maria_vladykin@gmx.de>
2024-11-18 10:53:41 +0100

    feat: activation maximization

--- helpers/activations.py
+++ helpers/activations.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
 from torch import Tensor, clamp, mean, randn
 from torch.nn import Module
 from torch.optim import Adam
+from tqdm import tqdm
 
 from helpers.cnn import ConvolutionalNeuralNetwork
 
@@ -33,7 +33,6 @@ def plot_conv_activations(activations: dict[str, Tensor], layer: str) -> None:
 
     # Plot the activation maps
     fig, axes = plt.subplots(num_filters // 8, 8, figsize=(15, num_filters * 2 // 8))
-    axes: Axes
 
     fig.suptitle(f"Filters of activation layer {layer}", fontsize=16)
     for idx in range(num_filters):
@@ -53,7 +52,7 @@ def plot_fc_activations(activations: dict[str, Tensor], layer: str):
     print(f"Shape of fc1 activations: {activation.shape}")
 
     # Plot the activations as a bar graph
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(4, 3))
     plt.bar(range(activation.shape[1]), activation[0].cpu().numpy())
     plt.title(f"Activations of {layer} Layer")
     plt.xlabel("Neuron Index")
@@ -61,44 +60,61 @@ def plot_fc_activations(activations: dict[str, Tensor], layer: str):
     plt.show()
 
 
-def activation_maximization(
-    model: ConvolutionalNeuralNetwork,
+def filter_activation_maximization(
     layer: Module,
-    layer_name: str,
-    filter_index: int,
-    input_size: tuple[int, int, int, int] = (1, 1, 28, 28),
+    model: ConvolutionalNeuralNetwork,
+    input_size: tuple[int, int, int, int] = (1, 1, 404, 303),
     lr: float = 0.1,
     iterations: int = 30,
 ):
-    input_image = randn(input_size, requires_grad=True)
-    # use Adam optimizer instead of SGD (better convergence)
-    optimizer = Adam([input_image], lr=lr, weight_decay=1e-6)
-    activations = {}
+    def maximize(filter_index: int):
+        input_image = randn(input_size, requires_grad=True)
+        # use Adam optimizer instead of SGD (better convergence)
+        optimizer = Adam([input_image], lr=lr, weight_decay=1e-6)
+        layer_activations = {}
 
-    def hook_function(module, input, output):
-        activations[layer_name] = output
+        def hook_function(module, input, output):
+            layer_activations["layer"] = output
 
-    # Register hook to capture activations on given layer
-    hook = layer.register_forward_hook(hook_function)
+        # Register hook to capture activations
+        hook = layer.register_forward_hook(hook_function)
 
-    for i in range(iterations):
-        # Zero gradients
-        optimizer.zero_grad()
+        for _ in range(iterations):
+            # Zero the parameter gradients after each iteration
+            optimizer.zero_grad()
+            # forward pass
+            model(input_image)
+            # Get activations from the layer (shape is batch_size, channels, height, width)
+            act = layer_activations["layer"][0, filter_index]
+            # Define loss as the negative mean of the activation
+            loss = -mean(act)
+            # Backward pass
+            loss.backward()
+            # Update the input image
+            optimizer.step()
+            # Clamp the input image to be between 0 and 1 (as we normalized the data to be between 0 and 1)
+            input_image.data = clamp(input_image.data, 0, 1)
 
-        # Utilize model to activate hook on layer
-        _ = model(input_image)
+        hook.remove()
+        return input_image.detach()
 
-        # Maximize activation
-        act = activations[layer_name][0, filter_index]
-        loss = -mean(act)
-        loss.backward()
-        optimizer.step()
+    # maximize each filter's activations
+    maximized_images = []
+    num_filters = 32
+    for filter_index in tqdm(range(num_filters)):
+        am_image = maximize(filter_index=filter_index)
+        maximized_images.append(am_image)
 
-        # Clamp the input image to be between 0 and 1 (as we normalized the data to be between 0 and 1)
-        input_image.data = clamp(input_image.data, 0, 1)
+    # Plot the activation maps
+    rows = num_filters // 8
+    fig, axes = plt.subplots(rows, 8, figsize=(15, rows * 2))
 
-        if (i + 1) % 10 == 0:
-            print(f"Iteration {i+1}/{iterations}, Loss: {-loss.item():.4f}")
+    fig.suptitle(f"Filters of activation layer {layer}", fontsize=16)
+    for idx, am_image in enumerate(maximized_images):
+        row = idx // 8
+        col = idx % 8
+        axes[row, col].imshow(am_image.squeeze().cpu().numpy(), cmap="gray")
+        axes[row, col].axis("off")
+        axes[row, col].set_title(f"Filter {idx}")
 
-    hook.remove()
-    return input_image.detach()
+    plt.show()
