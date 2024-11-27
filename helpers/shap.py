b903acdeebcc23eac7c61fd7aca1b19feca7877c
Maria Vladykin <maria_vladykin@gmx.de>
2024-11-27 16:28:49 +0100

    feat: added transformations for heatpoint placement comparison with SHAP, todo: fix plaid image bug

--- helpers/shap.py
+++ helpers/shap.py
@@ -1,11 +1,16 @@
-from numpy import swapaxes, transpose
-from shap import DeepExplainer, image_plot
-from torch import max, randperm
+import matplotlib.pyplot as plt
+from cv2 import COLOR_BGR2GRAY, COLOR_GRAY2BGR, cvtColor, line, rectangle
+from numpy import swapaxes, transpose, uint8, zeros
+from shap import DeepExplainer
+from shap.plots._image import image
+from torch import Tensor, empty, from_numpy
 from torch.utils.data import DataLoader
-from tqdm import tqdm
+from torchvision.transforms.v2.functional import gaussian_noise, rotate
+from tqdm.notebook import tqdm
 
 from helpers.cnn import ConvolutionalNeuralNetwork
 from helpers.dataset import BarkVN50Dataset
+from helpers.functions import one_hot_to_numeric_label_batch
 
 
 def shap_evaluate_cnn(
@@ -13,20 +18,63 @@ def shap_evaluate_cnn(
     train_dataset: BarkVN50Dataset,
     test_dataset: BarkVN50Dataset,
 ):
-    # get random 100 train_images and train an explainer with them
-    train_images_indexes = randperm(train_dataset.images.shape[0])[:100]
-    train_images = train_dataset.images[train_images_indexes]
+    def get_pred_label(input_images: Tensor) -> list[str]:
+        test_outputs = model(input_images)
+        pred_labels = one_hot_to_numeric_label_batch(test_outputs)
+        labels: list[str] = []
+        for i in range(pred_labels.__len__()):
+            labels.append(f"Pred: {pred_labels[i]}")
+        return labels
+
+    def generate_input_images(image: Tensor) -> Tensor:
+        input_images = empty(5, 1, 404, 303)
+
+        # original image
+        input_images[0] = image
+
+        # image with noise
+        input_images[1] = gaussian_noise(image)
+
+        # rotated iamge
+        input_images[2] = rotate(image, 30)
+
+        # image with black square
+        black_rectangle_image = cvtColor(image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
+        rectangle(black_rectangle_image, (100, 100), (200, 200), (0, 0, 0), -1)
+        input_images[3] = from_numpy(cvtColor(black_rectangle_image, COLOR_BGR2GRAY)).unsqueeze(0)
+
+        # image with plaid square
+        plaid_image = cvtColor(image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
+        plaid_texture = zeros((100, 100, 3), uint8)
+        for x in range(0, 100, 40):
+            line(plaid_texture, pt1=(0, x), pt2=(100, x), color=(220, 180, 110), thickness=5)
+            line(plaid_texture, pt1=(0, x + 7), pt2=(100, x + 7), color=(220, 180, 110), thickness=5)
+            line(plaid_texture, pt1=(x, 0), pt2=(x, 100), color=(150, 120, 90), thickness=5)
+            line(plaid_texture, pt1=(x + 9, 0), pt2=(x + 9, 100), color=(150, 120, 90), thickness=5)
+        plaid_image[100:200, 100:200] = plaid_texture
+        input_images[4] = from_numpy(cvtColor(plaid_image, COLOR_BGR2GRAY)).unsqueeze(0)
+
+        return input_images
+
+    # Get random 100 train_images and train an explainer with them
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    train_images, _ = next(iter(train_loader))
     explainer = DeepExplainer(model, train_images)
 
-    # calculate SHAP values for all test batches
-    test_loader = DataLoader(test_dataset, batch_size=4, shuffle=True)
+    # Calculate SHAP values for all test batches
+    test_loader = DataLoader(test_dataset, batch_size=10, shuffle=False)
     for test_images, targets in tqdm(test_loader):
-        # print label and prediction background
-        test_outputs = model(test_images)
-        print(f"true labels: {max(targets, 1)[1]}\npred labels: {max(test_outputs, 1)[1]}\npred values: {test_outputs}")
+        # Get 1 sample of each class and modify it to compare SHAP values
+        input_images = generate_input_images(test_images[0])
 
-        # calculate and plot SHAP values for test image batch
-        shap_values = explainer.shap_values(test_images)
-        shap_numpy = list(transpose(shap_values, (4, 0, 2, 3, 1)))
-        test_numpy = swapaxes(swapaxes(test_images.numpy(), 1, -1), 1, 2)
-        image_plot(shap_numpy, -test_numpy)
+        # Calculate and plot SHAP values for test image batch
+        shap_values = explainer.shap_values(input_images)
+        image(
+            shap_values=list(transpose(shap_values, (4, 0, 2, 3, 1))),
+            pixel_values=-swapaxes(swapaxes(input_images.cpu().numpy(), 1, -1), 1, 2),
+            true_labels=get_pred_label(input_images),
+            width=25,
+            show=False,
+        )
+        plt.suptitle(f"True label: {one_hot_to_numeric_label_batch(targets)[0]}")
+        plt.show()
