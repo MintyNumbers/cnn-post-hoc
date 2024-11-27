4ea33aa7d29dac7db118eabf5deb1d471fe03293
Maria Vladykin <maria_vladykin@gmx.de>
2024-11-27 22:11:39 +0100

    feat: instead of plaid pattern bigger black square

--- helpers/shap.py
+++ helpers/shap.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-from cv2 import COLOR_BGR2GRAY, COLOR_GRAY2BGR, cvtColor, line, rectangle
-from numpy import swapaxes, transpose, uint8, zeros
+from cv2 import COLOR_BGR2GRAY, COLOR_GRAY2BGR, cvtColor, rectangle
+from numpy import swapaxes, transpose
 from shap import DeepExplainer
 from shap.plots._image import image
 from torch import Tensor, empty, from_numpy
@@ -26,38 +26,32 @@ def shap_evaluate_cnn(
             labels.append(f"Pred: {pred_labels[i]}")
         return labels
 
-    def generate_input_images(image: Tensor) -> Tensor:
+    def generate_input_images(original_image: Tensor) -> Tensor:
         input_images = empty(5, 1, 404, 303)
 
         # original image
-        input_images[0] = image
+        input_images[0] = original_image
 
         # image with noise
-        input_images[1] = gaussian_noise(image)
+        input_images[1] = gaussian_noise(original_image)
 
         # rotated iamge
-        input_images[2] = rotate(image, 30)
+        input_images[2] = rotate(original_image, 30)
 
         # image with black square
-        black_rectangle_image = cvtColor(image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
-        rectangle(black_rectangle_image, (100, 100), (200, 200), (0, 0, 0), -1)
+        black_rectangle_image = cvtColor(original_image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
+        rectangle(black_rectangle_image, pt1=(100, 100), pt2=(200, 200), color=(0, 0, 0), thickness=-1)
         input_images[3] = from_numpy(cvtColor(black_rectangle_image, COLOR_BGR2GRAY)).unsqueeze(0)
 
         # image with plaid square
-        plaid_image = cvtColor(image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
-        plaid_texture = zeros((100, 100, 3), uint8)
-        for x in range(0, 100, 40):
-            line(plaid_texture, pt1=(0, x), pt2=(100, x), color=(220, 180, 110), thickness=5)
-            line(plaid_texture, pt1=(0, x + 7), pt2=(100, x + 7), color=(220, 180, 110), thickness=5)
-            line(plaid_texture, pt1=(x, 0), pt2=(x, 100), color=(150, 120, 90), thickness=5)
-            line(plaid_texture, pt1=(x + 9, 0), pt2=(x + 9, 100), color=(150, 120, 90), thickness=5)
-        plaid_image[100:200, 100:200] = plaid_texture
-        input_images[4] = from_numpy(cvtColor(plaid_image, COLOR_BGR2GRAY)).unsqueeze(0)
+        black_rectangle_image_2 = cvtColor(original_image.permute(1, 2, 0).numpy(), COLOR_GRAY2BGR)
+        rectangle(black_rectangle_image_2, pt1=(100, 100), pt2=(300, 300), color=(0, 0, 0), thickness=-1)
+        input_images[4] = from_numpy(cvtColor(black_rectangle_image_2, COLOR_BGR2GRAY)).unsqueeze(0)
 
         return input_images
 
     # Get random 100 train_images and train an explainer with them
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
     train_images, _ = next(iter(train_loader))
     explainer = DeepExplainer(model, train_images)
 
